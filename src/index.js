1ec6f10b0c46fd28da54106e900d049fd8bc15e4
uxwolf <135868251+uxwolf@users.noreply.github.com>
2025-08-01 13:55:08 +0530

    Added get user by id

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const { PORT } = require('./config/serverConfig');
 
 const apiRoutes = require("./routes/index");
 
+const UserRepository = require('./repository/user-repository');
+
 const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
@@ -14,8 +16,11 @@ const prepareAndStartServer = () => {
 
     app.use('/api',apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started on Port : ${PORT}`);
+        const repo = new UserRepository();
+        const response = await repo.getById(2);
+        console.log(response); 
     });
 }
 
--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -27,6 +27,19 @@ class UserRepository {
             throw error;
         }
     }
+
+    async getById(userId){
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: [ 'email', 'id'] 
+            });
+            return user;
+            
+        } catch (error) {
+            console.log("Something went wrong on repository");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserRepository;
